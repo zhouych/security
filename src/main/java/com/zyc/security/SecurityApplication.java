c251b9f99e11549f27d2d5a91a8ed3978300d0f6
zhouych <zhouych329@163.com>
2018-08-23 18:20:15 +0800

    ...

--- src/main/java/com/zyc/security/SecurityApplication.java
+++ src/main/java/com/zyc/security/SecurityApplication.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.zyc.baselibs.SpringContextUtils;
+
 @SpringBootApplication
 @MapperScan(basePackages = { "com.zyc.baselibs.dao", "com.zyc.security.dao" })
 public class SecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
+		SpringContextUtils.setApplicationContext(SpringApplication.run(SecurityApplication.class, args));
 	}
 }
--- src/main/java/com/zyc/security/dao/UserMapper.java
+++ src/main/java/com/zyc/security/dao/UserMapper.java
@@ -1,11 +1,14 @@
 package com.zyc.security.dao;
 
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.zyc.baselibs.dao.MybatisBaseMapper;
+import com.zyc.security.cache.RedisCache;
 import com.zyc.security.entities.User;
 
 
+@CacheNamespace(implementation = RedisCache.class,  flushInterval = 60000)
 @Mapper
 public interface UserMapper extends MybatisBaseMapper<User> {
 
