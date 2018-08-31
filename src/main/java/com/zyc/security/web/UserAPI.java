ef639b264a2fba2514ac198edb81ca1cf8fac015
zhouych <zhouych329@163.com>
2018-08-31 18:15:53 +0800

    ...

---
+++ src/main/java/com/zyc/security/web/UserAPI.java
@@ -0,0 +1,10 @@
+package com.zyc.security.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zyc.baselibs.web.BaseController;
+
+@RestController
+public class UserAPI extends BaseController {
+
+}
